e9bae5fcf94086cef975d9c8959d9455ce4d8725
Stanislav <stasvitalich1994@gmail.com>
2022-10-03 19:26:21 +0600

    Swapping the first and last rows of a two-dimensional array

--- Example 1/Program.cs
+++ Example 1/Program.cs
@@ -1,1 +1,80 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая меняет местами 
+// первую и последнюю строки двумерного массива.
+
+//Размерность массива: количество строк.
+int n = 0;
+
+//Размерность массива: количество столбцов. 
+int m = 0;
+
+Console.Write("Введите количество столбцов (M): ");
+// Пишем структуру, которая защитит нашу программу от поломки
+// в случае ввода пользователем строки, а не числа.
+bool parseMIsOk = int.TryParse(Console.ReadLine(), out int numberM);
+if (!parseMIsOk)
+{
+    Console.WriteLine("Введено значение некорректного формата");
+    return;
+}
+else
+{
+    m = numberM;
+}
+
+Console.Write("Введите количество строк (N): ");
+// Пишем структуру, которая защитит нашу программу от поломки
+// в случае ввода пользователем строки, а не числа.
+bool parseNIsOk = int.TryParse(Console.ReadLine(), out int numberN);
+if (!parseNIsOk)
+{
+    Console.WriteLine("Введено значение некорректного формата");
+    return;
+}
+else
+{
+    n = numberN;
+}
+
+// Задаём двумерный массив.
+int[,] array = new int[n, m];
+
+// Заполняем его рандомными числами.
+for (int i = 0; i < n; i = i + 1)
+{
+    for (int j = 0; j < m; j = j + 1)
+    {
+        array[i, j] = new Random().Next(1, 10);
+    }
+}
+
+// Выводим массив на печать.
+Console.WriteLine("");
+Console.WriteLine("Инициирован двумерный массив: ");
+for (int i = 0; i < n; i = i + 1)
+{
+    for (int j = 0; j < m; j = j + 1)
+    {
+        Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine("");
+}
+
+
+// Меняем элементы первой строки на элементы последней строки
+// через временную переменную.
+for (int i = 0; i < array.GetLength(1); i = i + 1)
+{
+    int temp = array [0, i];
+    array [0, i] = array [array.GetLength(0) - 1, i];
+    array [array.GetLength(0) - 1, i] = temp;
+}
+Console.WriteLine();
+
+for (int i = 0; i < n; i = i + 1)
+{
+    for (int j = 0; j < m; j = j + 1)
+    {
+        Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+}
